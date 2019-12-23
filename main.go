530663a04294922b1b00eb6ddcc950bcc2ed7c8c
limq <limq@cgnfs.com.cn>
2019-12-23 17:31:46 +0800

    2.5 type

--- main.go
+++ main.go
@@ -1,19 +1,33 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"strings"
+import "fmt"
+
+type Celsius float64    // 摄氏温度
+type Fahrenheit float64 // 华氏温度
+
+const (
+	AbsoluteZeroC Celsius = -273.15 // 绝对零度
+	FreezingC     Celsius = 0       // 结冰点温度
+	BoilingC      Celsius = 100     // 沸水温度
 )
 
-var n = flag.Bool("n", false, "omit trailing newline")
-var sep = flag.String("s", " ", "separator")
-
 func main() {
-	flag.Parse()
-	fmt.Println(strings.Join(flag.Args(), *sep))
-	fmt.Println(*n)
-	if !*n {
-		fmt.Println()
-	}
+	//fmt.Println(BoilingC - FreezingC)
+	//boilingF := CToF(BoilingC)
+	//fmt.Printf("%g\n",boilingF)
+	var a float64 = 100
+	var b float64 = 0
+	var c float64 = 36.5
+	var d float64 = 38
+	var e float64 = 42
+	fmt.Printf("摄氏温度%g度是华氏温度%g度\n", Celsius(a), CToF(Celsius(a)))
+	fmt.Printf("摄氏温度%g度是华氏温度%g度\n", Celsius(b), CToF(Celsius(b)))
+	fmt.Printf("摄氏温度%g度是华氏温度%g度\n", Celsius(c), CToF(Celsius(c)))
+	fmt.Printf("摄氏温度%g度是华氏温度%g度\n", Celsius(d), CToF(Celsius(d)))
+	fmt.Printf("摄氏温度%g度是华氏温度%g度\n", Celsius(e), CToF(Celsius(e)))
+
 }
+
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
